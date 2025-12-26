ac6188d19698308e0f91b9df3da9b17e88ca8685
longpv <long.phamvan@adamosoft.com>
2025-12-26 15:18:31 +0700

    base css js

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the footer
  *
@@ -11,24 +12,91 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'biotech' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'biotech' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'biotech' ), 'biotech', '<a href="http://underscores.me/">longpv</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<a href="<?php echo esc_url(__('https://wordpress.org/', 'biotech')); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf(esc_html__('Proudly powered by %s', 'biotech'), 'WordPress');
+			?>
+		</a>
+		<span class="sep"> | </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf(esc_html__('Theme: %1$s by %2$s.', 'biotech'), 'biotech', '<a href="http://underscores.me/">longpv</a>');
+		?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
+
+<script>
+	var read_more = "Xem thêm";
+</script>
+
+
+<div class="modal fade" id="videoModal" tabindex="-1" role="dialog" aria-labelledby="videoModal" aria-hidden="true" style="z-index:99999;">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+				<div><iframe width="100%" height="350" src="" frameborder="0" allowfullscreen=""></iframe></div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php wp_footer(); ?>
 
+
+
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$('#menu_top').hcSticky({
+			top: -1,
+			bottomEnd: 100,
+			wrapperClassName: 'sidebar-sticky'
+		});
+
+		$('#menu_mobile').hcSticky({
+			top: 1,
+			bottomEnd: 100,
+			wrapperClassName: 'sidebar-sticky'
+		});
+
+		autoPlayYouTubeModal();
+
+		function autoPlayYouTubeModal() {
+
+			var trigger = $("body").find('[data-toggle="modal"]');
+
+			trigger.click(function() {
+
+				var theModal = $(this).data("target"),
+
+					videoSRC = $(this).attr("data-theVideo"),
+
+					videoSRCauto = videoSRC + "?autoplay=1";
+
+				$(theModal + ' iframe').attr('src', videoSRCauto);
+
+				$(theModal + ' button.close').click(function() {
+
+					$(theModal + ' iframe').attr('src', videoSRC);
+
+				});
+
+			});
+		}
+
+		$('#inner-content-div').slimScroll({
+			height: '200px',
+			wheelStep: 20,
+			alwaysVisible: true
+		});
+
+	});
+</script>
+
 </body>
+
 </html>
--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * biotech functions and definitions
  *
@@ -7,9 +8,9 @@
  * @package biotech
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+if (! defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +20,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function biotech_setup() {
+function biotech_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on biotech, use a find and replace
 		* to change 'biotech' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'biotech', get_template_directory() . '/languages' );
+	load_theme_textdomain('biotech', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +39,19 @@ function biotech_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'biotech' ),
+			'menu-1' => esc_html__('Primary', 'biotech'),
 		)
 	);
 
@@ -83,7 +85,7 @@ function biotech_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +102,7 @@ function biotech_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'biotech_setup' );
+add_action('after_setup_theme', 'biotech_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +111,24 @@ add_action( 'after_setup_theme', 'biotech_setup' );
  *
  * @global int $content_width
  */
-function biotech_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'biotech_content_width', 640 );
+function biotech_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('biotech_content_width', 640);
 }
-add_action( 'after_setup_theme', 'biotech_content_width', 0 );
+add_action('after_setup_theme', 'biotech_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function biotech_widgets_init() {
+function biotech_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'biotech' ),
+			'name'          => esc_html__('Sidebar', 'biotech'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'biotech' ),
+			'description'   => esc_html__('Add widgets here.', 'biotech'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +136,20 @@ function biotech_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'biotech_widgets_init' );
+add_action('widgets_init', 'biotech_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function biotech_scripts() {
-	wp_enqueue_style( 'biotech-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'biotech-style', 'rtl', 'replace' );
+function biotech_scripts()
+{
+	wp_enqueue_style('biotech-style', get_stylesheet_uri(), array(), _S_VERSION);
 
-	wp_enqueue_script( 'biotech-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'biotech_scripts' );
+add_action('wp_enqueue_scripts', 'biotech_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,13 +174,85 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
 /**
  * Load WooCommerce compatibility file.
  */
-if ( class_exists( 'WooCommerce' ) ) {
+if (class_exists('WooCommerce')) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+// nhúng css vào
+/**
+ * Enqueue CSS
+ */
+function biotech_enqueue_styles()
+{
+
+	$css_uri = get_template_directory_uri() . '/dist/css/';
+
+	wp_enqueue_style('dashicons');
+
+	// ✅ Bootstrap CSS (BẮT BUỘC)
+	wp_enqueue_style(
+		'bootstrap',
+		$css_uri . 'bootstrap.min.css',
+		[],
+		_S_VERSION
+	);
+
+	wp_enqueue_style('cptch-stylesheet',       $css_uri . 'front_end_style.css', [], _S_VERSION);
+	wp_enqueue_style('cptch-desktop-style',     $css_uri . 'desktop_style.css', [], _S_VERSION);
+	wp_enqueue_style('contact-form-7',          $css_uri . 'styles.css', [], _S_VERSION);
+	wp_enqueue_style('wpdm-bootstrap',           $css_uri . 'bootstrap.css', [], _S_VERSION);
+	wp_enqueue_style('math-captcha-frontend',   $css_uri . 'frontend.css', [], _S_VERSION);
+	wp_enqueue_style('qts-front-styles',         $css_uri . 'qts-default.css', [], _S_VERSION);
+
+	wp_enqueue_style('font-awesome',             $css_uri . 'font-awesome.min.css', [], _S_VERSION);
+	wp_enqueue_style('menu-mobile',              $css_uri . 'menu_mobile.css', [], _S_VERSION);
+	wp_enqueue_style('style-media',               $css_uri . 'style_media.css', [], _S_VERSION);
+	wp_enqueue_style('style-custom',              $css_uri . 'style-custom.css', ['style-media'], _S_VERSION);
+	wp_enqueue_style('main-style',                $css_uri . 'style.css', [], _S_VERSION);
+	wp_enqueue_style('jssor',                     $css_uri . 'jssor.css', [], _S_VERSION);
+}
+add_action('wp_enqueue_scripts', 'biotech_enqueue_styles');
+
+/**
+ * Enqueue JS
+ */
+function biotech_enqueue_scripts()
+{
+
+	$js_uri = get_template_directory_uri() . '/dist/js/';
+
+	// Core
+	wp_enqueue_script('jquery');
+
+	// Libraries
+	wp_enqueue_script('jquery-form',  $js_uri . 'jquery.form.min.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('bootstrap',     $js_uri . 'bootstrap.min.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('hc-sticky',      $js_uri . 'jquery.hc-sticky.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('slimscroll',     $js_uri . 'jquery.slimscroll.min.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('owl-carousel',   $js_uri . 'owl.carousel.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('jssor-slider',   $js_uri . 'jssor.slider-24.1.5.min.js', [], _S_VERSION, true);
+	wp_enqueue_script('scripts-main',  $js_uri . 'scripts.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('postviews',     $js_uri . 'postviews-cache.js', ['jquery'], _S_VERSION, true);
+	wp_enqueue_script('gistfile',      $js_uri . 'gistfile1.js', ['jquery'], _S_VERSION, true);
+
+	// Localize
+	wp_localize_script(
+		'postviews',
+		'viewsCacheL10n',
+		[
+			'admin_ajax_url' => admin_url('admin-ajax.php'),
+			'post_id'        => get_the_ID(),
+		]
+	);
+
+	// Inline init JSSOR (SAU index.js)
+	wp_add_inline_script('index-js', 'jssor_1_slider_init();');
+}
+add_action('wp_enqueue_scripts', 'biotech_enqueue_scripts');
